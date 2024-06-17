92eff59314fa71a311f57e9136d727ec74f5a7ca
rtty12345.github.io <168611118+rtty12345@users.noreply.github.com>
2024-06-17 09:06:47 +0800

    Update Cfunction.js

--- jspvz_rtty/js/Cfunction.js
+++ jspvz_rtty/js/Cfunction.js
@@ -2208,9 +2208,14 @@ ShowMiniGame = function() {
 	PlayAudio("gravebutton");
 	SetBlock($("dMiniSmallContainer"))
 },
+
+ShowMiniGame2 = function() {
+	PlayAudio("gravebutton");
+	SetBlock($("dMiniSmallContainer2"))
+},
 	 
 HiddenMiniGame = function(a) { ! a && PlayAudio("tap");
-	SetNone($("dMiniSmallContainer"),$("dMiniSmallContainer"))
+	SetNone($("dMiniSmallContainer"),$("dMiniSmallContainer2"))
 },
 ShowRiddleGame = function() {
 	PlayAudio("gravebutton");
