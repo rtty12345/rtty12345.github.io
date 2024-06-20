bb744160f5e517008228fa2fa6b303192beed46e
rtty12345.github.io <168611118+rtty12345@users.noreply.github.com>
2024-06-20 13:50:17 +0800

    更新

---
+++ jspvz_rtty版/level/6.js
@@ -0,0 +1,40 @@
+oS.Init({
+	PName: [oPeashooter, oSunFlower, oCherryBomb, oWallNut, oPotatoMine],
+	ZName: [oZombie, oZombie2, oZombie3, oConeheadZombie, oPoleVaultingZombie],
+	PicArr: function() {
+		var a = oSnowPea.prototype,
+			b = a.PicArr;
+		return ["images/interface/background1.jpg", "images/interface/crater1.png", b[a.CardGif], b[a.NormalGif]]
+	}(),
+	backgroundImage: "images/interface/background1.jpg",
+	CanSelectCard: 0,
+	LevelName: "关卡 1-6",
+	LvlEName: 6,
+	LargeWaveFlag: {
+		10: $("imgFlag1")
+	}
+}, {
+	AZ: [
+		[oZombie, 3, 1],
+		[oZombie2, 2, 1],
+		[oZombie3, 2, 1],
+		[oConeheadZombie, 2, 1],
+		[oPoleVaultingZombie, 1, 2]
+	],
+	FlagNum: 10,
+	FlagToSumNum: {
+		a1: [3, 5, 9],
+		a2: [1, 3, 7, 15]
+	},
+	FlagToMonitor: {
+		9: [ShowFinalWave, 0]
+	},
+	FlagToEnd: function() {
+		NewImg("imgSF", "images/Card/Plants/SnowPea.png", "left:827px;top:525px;clip:rect(auto,auto,60px,auto)", EDAll, {
+			onclick: function() {
+				GetNewCard(this, oSnowPea, 7)
+			}
+		});
+		NewImg("PointerUD", "images/interface/PointerDown.gif", "top:490px;left:836px", EDAll)
+	}
+});
