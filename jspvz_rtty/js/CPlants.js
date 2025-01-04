67c2d6be9bd3d0ffc6723f21e8c7068f7fe658cd
jspvztiiiiiiiii <1825121802@qq.com>
2025-01-04 17:43:01 +0800

    Update CPlants.js

--- jspvz_rtty/js/CPlants.js
+++ jspvz_rtty/js/CPlants.js
@@ -529,11 +529,11 @@ oRepeater = InheritO(oPeashooter, {
 	height: 71,
 	BKind:-1,
 	beAttackedPointR: 53,
-	SunNum: 200,
+	SunNum: 250,
 	PicArr: ["images/Card/Plants/Repeater.png", "images/Plants/Repeater/0.gif", "images/Plants/Repeater/Repeater.gif", "images/Plants/PB-10.gif", "images/Plants/PeaBulletHit.gif"],
 	AudioArr: ["splat1", "splat2", "splat3", "plastichit", "shieldhit", "shieldhit2"],
 	Tooltip: "一次发射两颗寒冰豌豆",
-	Produce: '双发射手可以一次发射两颗豌豆<p>伤害：<font color="#FF0000">中等(每颗)</font><br>发射速度：<font color="#FF0000">两倍</font></p>双发射手很凶悍，他是在街头混大的。他不在乎任何人的看法，无论是植物还是僵尸，他打出豌豆，是为了让别人离他远点。其实呢，双发射手一直暗暗地渴望着爱情。',
+	Produce: '双发射手可以一次发射两颗寒冰豌豆<p>伤害：<font color="#FF0000">中等(每颗)</font><br>发射速度：<font color="#FF0000">两倍</font></p>双发射手很凶悍，他是在街头混大的。他不在乎任何人的看法，无论是植物还是僵尸，他打出豌豆，是为了让别人离他远点。其实呢，双发射手一直暗暗地渴望着爱情。',
 	NormalAttack1: oSnowPea.prototype.NormalAttack,
 	NormalAttack: function(a) {
 		this.NormalAttack1();
