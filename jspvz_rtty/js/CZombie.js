677a5f6d1b62cae45b07a6d25af4297fb6e03a96
jspvztiiiiiiiii <1825121802@qq.com>
2025-01-25 23:39:30 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -1488,7 +1488,7 @@ oNewspaperZombie = InheritO(OrnIIZombies, {
 			var j = CZombies.prototype,
 			i = k.OSpeed = k.LostPaperSpeed;
 			k.Altitude=1,
-			k.Attack=800;
+			k.Attack=k.LostPaperAttack;
 			k.ChkActs = j.ChkActs;
 			k.ChkActs1 = j.ChkActs1;
 			k.Speed && (k.Speed = !k.FreeSlowTime ? i: 0.5 * i);
@@ -1497,6 +1497,7 @@ oNewspaperZombie = InheritO(OrnIIZombies, {
 			}
 			PlayAudio("newspaper_rarrgh2");
 			k.EleBody.src = l;
+			k.Attack=k.LostPaperAttack;
 			k.JudgeAttack()
 		},
 		[h, f[[g.NormalGif = g.OrnLostNormalGif, g.AttackGif = g.OrnLostAttackGif][b]]]))
