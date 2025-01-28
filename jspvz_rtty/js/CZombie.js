c085e3b82fa74a98cfb80922003700ad60ad805f
jspvztiiiiiiiii <1825121802@qq.com>
2025-01-28 21:08:39 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -1218,7 +1218,25 @@ oConeheadZombie= InheritO(OrnIZombies, {
 	},
 	Produce: '他的路障头盔，使他两倍坚韧于普通僵尸。<p>韧性：<font color="#FF0000">中</font></p>和其他僵尸一样，路障头僵尸盲目地向前。但某些事物却使他停下脚步，捡起一个交通路障，并固实在自己的脑袋上。是的，他很喜欢参加聚会。'
 }),
-oBucketheadZombie = InheritO(oConeheadZombie,{
+oConeheadZombie1= InheritO(OrnIZombies, {
+	EName: "oConeheadZombie",
+	CName: "路障僵尸",
+	OrnHP: 500,
+	Lvl: 2,
+	SunNum: 75,
+	StandGif: 11,
+	PicArr: (function() {
+		var b = "images/Zombies/ConeheadZombie/",
+		a = "images/Zombies/Zombie/";
+		return ["images/Card/Zombies/ConeheadZombie.png", b + "0.gif", b + "ConeheadZombie.gif", b + "ConeheadZombieAttack.gif", a + "ZombieLostHead.gif", a + "ZombieLostHeadAttack.gif", a + "ZombieHead.gif" + $Random, a + "ZombieDie.gif" + $Random, a + "BoomDie.gif" + $Random, a + "Zombie.gif", a + "ZombieAttack.gif", b + "1.gif"]
+	})(),
+	AudioArr: ["plastichit"],
+	PlayNormalballAudio: function() {
+		PlayAudio("plastichit")
+	},
+	Produce: '他的路障头盔，使他两倍坚韧于普通僵尸。<p>韧性：<font color="#FF0000">中</font></p>和其他僵尸一样，路障头僵尸盲目地向前。但某些事物却使他停下脚步，捡起一个交通路障，并固实在自己的脑袋上。是的，他很喜欢参加聚会。'
+}),
+oBucketheadZombie= InheritO(oConeheadZombie1,{
 	EName: "oBucketheadZombie",
 	CName: "铁桶僵尸",
 	OrnHP: 1000,
@@ -1251,7 +1269,7 @@ oBucketheadZombie = InheritO(oConeheadZombie,{
 		11 : "images/Zombies/BucketheadZombie/1.gif"
 	},
 }),
-oFootballZombie = InheritO(oConeheadZombie,{
+oFootballZombie = InheritO(oConeheadZombie1,{
 	EName: "oFootballZombie",
 	CName: "橄榄球僵尸",
 	OrnHP: 2000,
