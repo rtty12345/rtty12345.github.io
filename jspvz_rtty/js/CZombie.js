12f810ac23df850d480e78fe3e43a3f0a1c5ca4d
jspvztiiiiiiiii <1825121802@qq.com>
2025-02-06 10:17:45 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -2664,8 +2664,7 @@ oZomboni = function() {
 			c.ExchangeLR(c, 1);
 			c.JudgeAttack = c.JudgeAttackH;
 			c.PZ = 0;
-			c.ClearChild($("dIceCar" + f));
-			c.Attack=1000;
+			c.Attack=3000;
 			c.WalkDirection = 1;
 			c.ZX = c.AttackedRX;
 			c.ChkActs = c.ChkActs1;
