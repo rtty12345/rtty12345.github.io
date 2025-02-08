13b99ab4c4e4c2fb7fed20d9bcaf0c58fac5fd37
jspvztiiiiiiiii <1825121802@qq.com>
2025-02-08 10:25:51 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -1345,6 +1345,15 @@ oConeheadZombie= InheritO(OrnIZombies, {
 	PlayNormalballAudio: function() {
 		PlayAudio("plastichit")
 	},
+	PrivateAct: function(a){
+            if(!a.Change){
+                a.Change = true;
+                oSym.addTask(200,function(a){
+                    a.ChangeR(a);
+                    a.Change = false;
+                },[a])
+            }
+        },
 	Produce: '他的路障头盔，使他两倍坚韧于普通僵尸。<p>韧性：<font color="#FF0000">中</font></p>路障僵尸在聚会上找到了一份给舞王伴舞的工作，薪水不错，虽然路障僵尸脱帽又戴帽的样子很招笑，但他们的冲击力的确是顶',
 		ChangeChkActsTo0: function(c, b, a) {
 		if (!c.PZ) {
@@ -1506,15 +1515,6 @@ oPoleVaultingZombie = InheritO(OrnNoneZombies, {
 		return ["images/Card/Zombies/PoleVaultingZombie.png", a + "0.gif", a + "PoleVaultingZombie.gif", a + "PoleVaultingZombieAttack.gif", a + "PoleVaultingZombieLostHead.gif", a + "PoleVaultingZombieLostHeadAttack.gif", a + "PoleVaultingZombieHead.gif" + $Random, a + "PoleVaultingZombieDie.gif" + $Random, a + "BoomDie.gif" + $Random, a + "PoleVaultingZombieWalk.gif", a + "PoleVaultingZombieLostHeadWalk.gif", a + "PoleVaultingZombieJump.gif", a + "PoleVaultingZombieJump2.gif", a + "1.gif"]
 	})(),
 	AudioArr: ["polevault", "grassstep"],
-	PrivateAct: function(a){
-            if(!a.Change){
-                a.Change = true;
-                oSym.addTask(150,function(a){
-                    a.ChangeR(a);
-                    a.Change = false;
-                },[a])
-            }
-        },
 	Produce: '撑杆僵尸可以跳N格<p>韧性：<font color="#FF0000">中(600)</font><Br>速度：<font color="#FF0000">快,而后慢(跳跃后)</font><BR>特点：<font color="#FF0000">跃过他所碰到的第一筑植物</font></p>一些僵尸渴望走得更远、得到更多，这也促使他们由普通成为非凡。那就是撑杆僵尸。',
         getShadow: function(a) {
             return "left:" + (a.beAttackedPointL - 20) + "px;top:" + (a.height - 35) + "px"
