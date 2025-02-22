fb24c1079759625b0a231699b0513823dc6a2156
jspvztiiiiiiiii <1825121802@qq.com>
2025-02-22 20:38:25 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -3725,7 +3725,7 @@ oSquashZombie = InheritO(oConeheadZombie1, {
             11: "images/Zombies/BucketheadZombie/1.gif"
         }
     }),
-    oPeaZombie = InheritO(oNewspaperZombie, {
+    oPeaZombie= InheritO(oNewspaperZombie1, {
         EName: "oPeaZombie",
         CName: "豌豆僵尸",
         OrnHP:10000,
