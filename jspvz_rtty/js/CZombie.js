8d139caa7804974099fb6f9ceb9d193259727ec7
jspvztiiiiiiiii <1825121802@qq.com>
2025-03-16 16:38:31 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -4270,7 +4270,7 @@ oSquashZombie = InheritO(oConeheadZombie1, {
                 var p = oGd.$[`${a.R}_${C}_1`];
                 if(p && p.canEat && (p.EName != "oPotatoMine" && p.EName != "oCherryBomb" && p.EName != "oJalapeno" && p.EName != "oDoomShroom")){
                     a.bool = 1;
-                    a.getHurt(C,2,C.Attack);
+                    a.Die;
                     PlayAudio("gargantuar_thump");
                 }
             }
