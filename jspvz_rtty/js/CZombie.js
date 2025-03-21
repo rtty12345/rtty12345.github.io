3bc35e18f0109269fcd4b17546c67fcdd0d740c4
jspvztiiiiiiiii <1825121802@qq.com>
2025-03-21 20:18:28 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -1209,8 +1209,9 @@ oFlagZombie = InheritO(oZombie, {
 	CName: "处决者旗帜",
 	OSpeed: 10,
 	Speed: 10,
-	SunNum: 325,
+	SunNum: 275,
 	Attack:500,
+	AKind:2,
 	HP:400,
 	BreakPoint:1,
 	beAttackedPointR: 101,
@@ -2562,7 +2563,7 @@ oNewspaperZombie2= InheritO(OrnIIZombies, {
 		g.ChkActs1 = function() {
 			return 1
 		},
-		g.EleBody.src = f[g.LostPaperGif] + $Random + Math.random(), g.Ornaments = 0, g.LostHeadGif = 8, g.LostHeadAttackGif = 9,g.getFirePea = e.getFirePea,g.getSnowPea = e.getSnowPea,g.getSlowPea = e.getSlowPea,g.getHit0=g.getHit1=g.getHit2=g.getHit3=e.getHit,g.Altitude=3,g.Attack=Infinity,oSym.addTask(300,
+		g.EleBody.src = f[g.LostPaperGif] + $Random + Math.random()， g.Ornaments = 0, g.LostHeadGif = 8, g.LostHeadAttackGif = 9,g.getFirePea = e.getFirePea,g.getSnowPea = e.getSnowPea,g.getSlowPea = e.getSlowPea,g.getHit0=g.getHit1=g.getHit2=g.getHit3=e.getHit,g.Altitude=3,g.Attack=Infinity,g.AKind=2,oSym.addTask(300,
 		function(m, l) {
 			var k = $Z[m];
 			if (!k) {
