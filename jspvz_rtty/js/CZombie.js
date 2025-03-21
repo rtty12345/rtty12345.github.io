6f9a2195c507d65b7de03a9b49f0949082b57e1b
jspvztiiiiiiiii <1825121802@qq.com>
2025-03-21 20:57:39 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -1211,7 +1211,6 @@ oFlagZombie = InheritO(oZombie, {
 	Speed: 10,
 	SunNum: 275,
 	Attack:500,
-	AKind:2,
 	HP:400,
 	BreakPoint:1,
 	beAttackedPointR: 101,
@@ -2563,7 +2562,7 @@ oNewspaperZombie2= InheritO(OrnIIZombies, {
 		g.ChkActs1 = function() {
 			return 1
 		},
-		g.EleBody.src = f[g.LostPaperGif] + $Random + Math.random(),g.Ornaments = 0, g.LostHeadGif = 8, g.LostHeadAttackGif = 9,g.getFirePea = e.getFirePea,g.getSnowPea = e.getSnowPea,g.getSlowPea = e.getSlowPea,g.getHit0=g.getHit1=g.getHit2=g.getHit3=e.getHit,g.Altitude=3,g.Attack=Infinity,g.AKind=2,oSym.addTask(300,
+		g.EleBody.src = f[g.LostPaperGif] + $Random + Math.random(),g.Ornaments = 0, g.LostHeadGif = 8, g.LostHeadAttackGif = 9,g.getFirePea = e.getFirePea,g.getSnowPea = e.getSnowPea,g.getSlowPea = e.getSlowPea,g.getHit0=g.getHit1=g.getHit2=g.getHit3=e.getHit,g.Altitude=3,g.Attack=Infinity,oSym.addTask(300,
 		function(m, l) {
 			var k = $Z[m];
 			if (!k) {
@@ -2653,10 +2652,8 @@ oNewspaperZombie3= InheritO(OrnIIZombies, {
             callback && callback();
         },
         PrivateBirth: function(a){
-            let z = $(a.id);
-            z.PeaHead = "Pea" + Math.random();
-            let pea = NewImg(z.PeaHead,"images/Plants/GatlingPea/GatlingPea.gif","position:absolute;width:80px;height:80px;transform:rotateY(180deg);left:45px;top:30px;",0);
-            z.appendChild(pea);
+            a.PrivateAct = 
+		    Math.round(Math.Random()*1+0)? a.PrivateAct1 : a.PrivateAct2;
         },
         PrivateAct: function(){
             let a = this;
