5ab604b6f1e616ee9d936782413551623cc02f98
jspvztiiiiiiiii <1825121802@qq.com>
2025-03-28 06:59:02 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -722,7 +722,7 @@ oBackupDancer = InheritO(OrnNoneZombies, {
 			CustomTop: 400 - g.height + g.GetDY()
 		})), g.ZX = g.AttackedLX -= b, g.Ele.style.left = Math.floor(g.X -= b) + "px", f = 1)) : f = 1) : f = 1;
 		g.ChkSpeed(g);
-		this.PrivateAct&&this.PrivateAct(this);
+		this.PrivateAct(this);
 		return f
 	},
 	ChkSpeed: function(b) {
