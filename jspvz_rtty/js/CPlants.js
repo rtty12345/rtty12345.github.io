1e0c981b87cdedc826f48dce5f3a99936f6cd939
jspvztiiiiiiiii <1825121802@qq.com>
2025-04-05 17:25:31 +0800

    Update CPlants.js

--- jspvz_rtty/js/CPlants.js
+++ jspvz_rtty/js/CPlants.js
@@ -720,7 +720,7 @@ oRepeater = InheritO(oPeashooter, {
 			var c = $P[d];
 			c && c.NormalAttack1(); --b && oSym.addTask(15, arguments.callee, [d, b])
 		},
-		[this.id,Math.round(Math.random()*3+2)])
+		[this.id,Math.round(Math.random()*2+1)])
 	}	
 }),
 oPeashooter1= InheritO(CPlants, {
@@ -824,8 +824,8 @@ oGatlingPea1= InheritO(CPlants, {
 	coolTime: 50,
 	PicArr: ["images/Card/Plants/GatlingPea.png","images/Plants/GatlingPea/0.gif","images/Plants/GatlingPea/GatlingPea.gif","images/Plants/PB10.gif","images/Plants/PeaBulletHit.gif"],
 	AudioArr: ["splat1", "splat2", "splat3", "plastichit", "shieldhit", "shieldhit2"],
-	Tooltip: "一次发射八颗真正的火豌豆<br>(需要双发射手)",
-	Produce: '加特林可以一次发射八颗真正的火豌豆<p>伤害：<font color="#FF0000">高(每颗)</font><br>发射速度：<font color="#FF0000">六倍<br>只能种在双发射手上</font></p>当加特林宣布他要参军的时候，他的父母很为他担心，他们异口同声地对他说：“亲爱的，但这太危险了。”加特林拒绝让步，“生活本就危险，”他这样回答着，此时他的眼睛里，正闪烁着钢铁般的信念。',
+	Tooltip: "一次散射六颗真正的火豌豆<br>(需要双发射手)",
+	Produce: '加特林可以一次散射六颗真正的火豌豆<p>伤害：<font color="#FF0000">高(每颗)</font><br>发射速度：<font color="#FF0000">六倍<br>只能种在双发射手上</font></p>当加特林宣布他要参军的时候，他的父母很为他担心，他们异口同声地对他说：“亲爱的，但这太危险了。”加特林拒绝让步，“生活本就危险，”他这样回答着，此时他的眼睛里，正闪烁着钢铁般的信念。',
 	CanGrow: function(b, a, d) {
 		var c = b[1];
 		return c && c.EName == "oRepeater"
@@ -911,7 +911,7 @@ oGatlingPea1= InheritO(CPlants, {
 			var c = $P[d];
 			c && c.NormalAttack1(); --b && oSym.addTask(10, arguments.callee, [d, b])
 		},
-		[this.id,5])
+		[this.id,Math.round(Math.random()*2+3)])
 	}	
 }),
 oSplitPea = InheritO(oPeashooter, {
