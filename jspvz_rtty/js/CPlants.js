953839edceabea3d36fcf24a433b04fe2d2f2adb
jspvztiiiiiiiii <1825121802@qq.com>
2025-04-06 13:45:32 +0800

    Update CPlants.js

--- jspvz_rtty/js/CPlants.js
+++ jspvz_rtty/js/CPlants.js
@@ -774,7 +774,7 @@ oGatlingPea= InheritO(oPeashooter, {
 	canEat:0,
 	BKind:-1,
 	coolTime: 50,
-	PicArr: ["images/Card/Plants/GatlingPea.png", "images/Plants/GatlingPea/0.gif", "images/Plants/GatlingPea/GatlingPea.gif", "images/Plants/PB10.gif", "images/Plants/PeaBulletHit.gif"],
+	PicArr: ["images/Card/Plants/GatlingPea.png", "images/Plants/GatlingPea/0.gif", "images/Plants/GatlingPea/GatlingPea.gif", "images/Plants/PB-10.gif", "images/Plants/PeaBulletHit.gif"],
 	AudioArr: ["splat1", "splat2", "splat3", "plastichit", "shieldhit", "shieldhit2"],
 	Tooltip: "一次发射四颗真正的冰豌豆<br>(需要双发射手)，40秒后成长为霰弹枪射手，期间不会受到任何形式伤害",
 	Produce: '加特林可以一次发射四颗冰豌豆，40秒后成长为霰弹枪射手，期间不会受到任何形式的伤害<p>伤害：<font color="#FF0000">高(每颗)</font><br>发射速度：<font color="#FF0000"> 4~6倍<br>只能种在双发射手上</font></p>当加特林宣布他要参军的时候，他的父母很为他担心，他们异口同声地对他说：“亲爱的，但这太危险了。”加特林拒绝让步，“生活本就危险，”他这样回答着，此时他的眼睛里，正闪烁着钢铁般的信念。',
