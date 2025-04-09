241e627566c43ef5fda74c9e70e3de0d3cb0532c
jspvztiiiiiiiii <1825121802@qq.com>
2025-04-09 21:20:40 +0800

    Update CPlants.js

--- jspvz_rtty/js/CPlants.js
+++ jspvz_rtty/js/CPlants.js
@@ -889,8 +889,8 @@ oGatlingPea1= InheritO(CPlants, {
 			[h]);
 			oSym.addTask(1,
 			function(n, l, m, k, i, j) {
-				j(oZ.getZ0(n, l), 7, i) && ((n += 4) > 900 || (k -= 3) < -15 ? ClearChild(i) : (SetStyle(i, {
-					left: (m += 6) + "px",
+				j(oZ.getZ0(n, l), 7, i) && ((n += 6) > 900 || (k -= 6) < -15 ? ClearChild(i) : (SetStyle(i, {
+					left: (m +=Math.random()*2+1)  + "px",
 					top: k + "px"
 				}), oSym.addTask(1, arguments.callee, [n, GetR(k + 15), m, k, i, j])))
 			},
@@ -907,7 +907,7 @@ oGatlingPea1= InheritO(CPlants, {
 			[h]);
 			oSym.addTask(1,
 			function(n, l, m, k, i, j) {
-				j(oZ.getZ0(n, l), 7, i) && ((n += 4) > 900 || (k -= 3) < -15 ? ClearChild(i) : (SetStyle(i, {
+				j(oZ.getZ0(n, l), 7, i) && ((n += 4) > 900 || (k -= 4) < -15 ? ClearChild(i) : (SetStyle(i, {
 					left: (m += 4) + "px",
 					top: k + "px"
 				}), oSym.addTask(1, arguments.callee, [n, GetR(k + 15), m, k, i, j])))
@@ -925,8 +925,8 @@ oGatlingPea1= InheritO(CPlants, {
 			[h]);
 			oSym.addTask(1,
 			function(n, l, m, k, i, j) {
-				j(oZ.getZ0(n, l), 1, i) && ((n += 4) > 900 || (k += 3) > 600 ? ClearChild(i) : (SetStyle(i, {
-					left: (m += 6) + "px",
+				j(oZ.getZ0(n, l), 1, i) && ((n += 6) > 900 || (k += 6) > 600 ? ClearChild(i) : (SetStyle(i, {
+					left: (m +=Math.random()*2+1) + "px",
 					top: k + "px"
 				}), oSym.addTask(1, arguments.callee, [n, GetR(k + 15), m, k, i, j])))
 			},
@@ -943,7 +943,7 @@ oGatlingPea1= InheritO(CPlants, {
 			[h]);
 			oSym.addTask(1,
 			function(n, l, m, k, i, j) {
-				j(oZ.getZ0(n, l), 1, i) && ((n += 4) > 900 || (k += 3) > 600 ? ClearChild(i) : (SetStyle(i, {
+				j(oZ.getZ0(n, l), 1, i) && ((n += 4) > 900 || (k += 4) > 600 ? ClearChild(i) : (SetStyle(i, {
 					left: (m += 4) + "px",
 					top: k + "px"
 				}), oSym.addTask(1, arguments.callee, [n, GetR(k + 15), m, k, i, j])))
