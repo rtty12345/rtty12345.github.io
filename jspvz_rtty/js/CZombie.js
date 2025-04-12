d0b4db7dc0537fa87e62912483aafa52bca8f22a
jspvztiiiiiiiii <1825121802@qq.com>
2025-04-12 10:44:34 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -1934,8 +1934,51 @@ oBucketheadZombie= InheritO(oConeheadZombie,{
 	PrivateAttack:function(){
             this.HP += 60;
     }, 
-	PrivateAct: function(a){
-            if(a.OrnHP<= 0){
+	PrivateAct: function(){
+	let a = this;
+            if(!a.bool){
+                a.bool = 1;
+                oSym.addTask(175,function(a){
+                    let i = 0,max = 3;
+                    var timer = setInterval(function(){
+                i++;
+                let z = $(a.id);
+                let div = $n("div");
+                let d = "Pea" + Math.random();
+                div.id = d;
+                div.innerHTML = '<img src="images/Plants/PB00.gif">';
+                EditEle(div,0,{
+                    position:"absolute",
+                    zIndex:"24",
+                    left:z.offsetLeft + "px",
+                    top:z.offsetTop + 40 + "px"
+                },EDPZ,0);
+                oSym.addTask(1,function(z,d,a){
+                try{
+                $(d).style.left = $(d).offsetLeft - 5 + "px";
+                let pea = $(d);
+                let C = GetC(z.offsetLeft + 40);
+                for(let i = 3;i >= 0;i--){
+                    for(let j = 1;j <= C;j++){
+                        let p = oGd.$[a.R+"_"+j+"_"+i];
+                        p && (p.canEat) && (p.EName != "oLawnCleaner" && p.EName != "oPoolCleaner" && p.EName != "oBrains" && p.EName != "oPuffShroom" && p.EName != "oSunShroom" && p.EName != "oPotatoMine" && p.EName != "oCherryBomb" && p.EName != "oJalapeno" && p.EName != "oDoomShroom"&& p.EName != "oSunFlower") && (($(p.id).offsetLeft + $(p.id).offsetWidth >= $(d).offsetLeft) && ($(p.id).offsetLeft >= $(d).offsetLeft + $(d).offsetWidth)) && (PlayAudio("splat1"),(p.HP -= 20),($(d) && ClearChild($(d))));
+                        p && (p.canEat) && (p.HP <= 0) && p.Die();
+                    }
+                }
+                if($(d).offsetLeft <= 0){
+                    ClearChild($(d));
+                    $(d).isDie = true;
+                }
+                !($(d).isDie) && oSym.addTask(1,arguments.callee,[z,d,a])
+                }catch(e){
+                }
+            },[z,d,a]);
+                (i > max) && clearInterval(timer);
+            },100)
+                    !a.isDie && (a.HP > 90) && oSym.addTask(125,arguments.callee,[a]);
+                },[a]);
+            },
+        if(a.OrnHP<= 0){
 		a.Speed=8;
                 }
         },
