fc1d46b47c8ff1e0949f2bff47aee0b047f220ac
jspvztiiiiiiiii <1825121802@qq.com>
2025-04-16 22:39:08 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -3016,7 +3016,7 @@ oNewspaperZombie2= InheritO(OrnIIZombies, {
 		},
 	PrivateAct:function(a){
             if(!a.bool){
-	        for (let i = 0; i <= 15; i++) {
+	        for (let i = 0; i <= 20; i++) {
                 oP.SetTimeoutTomZombie([oNewspaperZombie]);
 		a.bool=1;
               }
