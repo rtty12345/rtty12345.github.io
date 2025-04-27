402b37a61a965a83b41e78fc33fe51aa4a6fc7d0
jspvztiiiiiiiii <1825121802@qq.com>
2025-04-27 22:09:00 +0800

    Update CPlants.js

--- jspvz_rtty/js/CPlants.js
+++ jspvz_rtty/js/CPlants.js
@@ -2025,11 +2025,10 @@ oFumeShroom = InheritO(CPlants, {
 		while (e--) { (g = d[e]).Altitude < 2 && g.getHit1(g, 30);
 		var t= "Snow_" + Math.random();
 		for(t in $Z)
-		Math.round(Math.random()*100)>7?g.getSlow(g):g.getFreeze(g,t),oSym.addTask(40,
+		Math.round(Math.random()*100)>7?g.getSlow(g):g.getFreeze(g,t),
 				function(s) {
 					ClearChild(s)
-				},
-				[this])
+				}
 		}
 		b.childNodes[1].src = "images/Plants/FumeShroom/FumeShroomAttack.gif";
 		SetVisible($(a));
