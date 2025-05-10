340dca50c65a600425e21d415d355b7979549af6
jspvztiiiiiiiii <1825121802@qq.com>
2025-05-10 18:19:08 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -5357,10 +5357,10 @@ oDiggerZombie = InheritO(OrnNoneZombies, {
     );
   },
 	OpenBox: function(b) {
-		b.beAttacked=b.isAttacking=0;
+		var a=$Z[b];
 		oSym.addTask(50,
 		function(c) {
-			$Z[c] && (a.Status = 0,PlayAudio("jack_surprise"), oSym.addTask(90,
+			$Z[c] && (a.Status = 0,a.beAttacked=a.isAttacking=0;PlayAudio("jack_surprise"), oSym.addTask(90,
 			function(f) {
 				var e = $Z[f],
 				d;
