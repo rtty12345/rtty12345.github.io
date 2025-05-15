9fb77e5e7f56b7f3ef78176d9c75211a7df4934e
jspvztiiiiiiiii <1825121802@qq.com>
2025-05-16 06:40:30 +0800

    Create 二爷海.js

---
+++ jspvz_rtty/level/二爷海.js
@@ -0,0 +1,32 @@
+oS.Init({
+    PName: ShengPlantList,
+    ZName: [oNewspaperZombie],
+    PicArr: ["images/interface/background1.jpg"], 
+    backgroundImage: "images/interface/background1.jpg",
+	Coord:1,
+    DKind: 1,
+	LF:[0,1,1,1,1,1,1],
+	//ZF:[0,0,0,2,2,0,0],
+    CanSelectCard: 1, 
+    LevelName: "二爷海",  
+    LvlEName: 11451,  
+    SunNum: 100,
+    LargeWaveFlag: {
+        10 : $("imgFlag3"),
+        20 : $("imgFlag1")
+     }
+},
+{
+    AZ: [
+    [oNewspaperZombie2,1,1,[1]]
+    ], 
+    FlagNum: 20,
+    FlagToSumNum: {
+        a1: [1,5,7,10,15,20],
+        a2: [50,80,140,180,250,300,500]
+    },
+    FlagToMonitor: {
+        9 : [ShowLargeWave, 0],
+        19 : [ShowFinalWave, 0]
+    }
+});
