dd98878800a4da83200999adb514239deff2c21b
jspvztiiiiiiiii <1825121802@qq.com>
2025-05-18 18:42:21 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -3575,7 +3575,7 @@ oScreenDoorZombie = InheritO(oNewspaperZombie1, {
             oSym.addTask(10, function(f, e) {
                 var h = $Z[f],
                     g;
-                h && h.beAttacked && !h.FreeFreezeTime && !h.FreeSetbodyTime && ((g = $P[e]) && g.getHurt(h,2,400),(CustomSpecial(oPuffShroom,h.R,h.C+1)),h.JudgeAttack())
+                h && h.beAttacked && !h.FreeFreezeTime && !h.FreeSetbodyTime && ((g = $P[e]) && g.getHurt(h,2,400),h.JudgeAttack())
             }, [d, c]);
             this.PrivateAttack && this.PrivateAttack(this)
         },
