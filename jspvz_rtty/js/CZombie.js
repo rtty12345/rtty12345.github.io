6c32e72d906ee971e8831ffa7438e1061fbb361f
jspvztiiiiiiiii <1825121802@qq.com>
2025-05-31 14:18:22 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -4799,7 +4799,9 @@ oDolphinRiderZombie1= InheritO(oDolphinRiderZombie, {
 				h.getFreeze = s.getFreeze;
 				h.getRaven = s.getRaven;
 				h.AttackZombie2 = s.AttackZombie2;
-				for(z in $Z) left= $Z[z];
+				for(z in $Z) left= $Z[z],A=left.Ele,left.ZX -= 80, left.AttackedLX -=80, left.AttackedRX -= 80, left.X -=80,SetStyle(A, {
+				left: d.X + "px"
+			});
 			};
 			h && ((k = $P[j]) && k.Stature > 0 ? (h.AttackedRX = (h.X = (h.AttackedLX = h.ZX = r = k.AttackedRX) - (h.beAttackedPointL = 45)) + (h.beAttackedPointR = 100), SetStyle(i, {
 				left: h.X + "px"
