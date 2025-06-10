cce7e6127bd2e472da267c984107587cd5c9dba1
jspvztiiiiiiiii <1825121802@qq.com>
2025-06-10 10:48:16 +0800

    Update CPlants.js

--- jspvz_rtty/js/CPlants.js
+++ jspvz_rtty/js/CPlants.js
@@ -2059,10 +2059,10 @@ oCoffeeBean = InheritO(CPlants, {
 			var d = oGd.$[c],
 			b;
 			d && (b = d.WakeUP, (!b ? ($(d.id).childNodes[1].src = d.PicArr[d.NormalGif], d.canTrigger = 1, d.Sleep = 0) : b(d)));
-			d.highwork=1;
+			d&&(d.highwork=1);
 			oSym.addTask(1000,function(c){
 			var d = oGd.$[c];
-			d.highwork=0
+			d&&(d.highwork=0)
 			},[c]);
 			a.Die()
 		},
