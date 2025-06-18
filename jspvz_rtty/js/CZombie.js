b3d34fe8dc2e2ce721a23073a8b59fcf285b44f8
jspvztiiiiiiiii <1825121802@qq.com>
2025-06-18 22:00:06 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -766,7 +766,7 @@ CheckBoomFire: function (f) {
         for (let j = 0; j < 4; j++) {
           let g = oGd.$[y + "_" + i + "_" + j];
 	  if(g&&g.EName!="oHypnoShroom"&& g.EName!="oGarlic"&&g.getHurt!=function(){}&&g.EName!="oPotatoMine"&&g.EName!="oSquash"){
-          g && g.getHurt(s,0,50)
+          g && g.getHurt(this,0,50)
 	  }else	if(g&&g.getHurt!=function(){}){
 	g&&g.Die()
 	  }
