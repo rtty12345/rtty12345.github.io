8b5422fe8bb69eadc880138c2bb3167eefcb7447
jspvztiiiiiiiii <1825121802@qq.com>
2025-06-29 16:51:54 +0800

    Update CPlants.js

--- jspvz_rtty/js/CPlants.js
+++ jspvz_rtty/js/CPlants.js
@@ -57,9 +57,9 @@
 		e.pixelTop = g;
 		e.pixelBottom = g + e.GetDBottom();
 		e.opacity = 1;
-	for (let A= ((e.R - 1) || 0); A<= (e.R+ 1 <= oS.R ? e.R+ 1 : oS.R); A++) {
+	for (let A= ((e.R - 1) || 0); A<= (e.R+ 1 <= oS.R ? e.R+ 1 : oS.R);A++) {
 		for (let B=e.C-1;B<=e.C+1;B++){
-oGd.$Plantern[A+"_"+B]&&(e.highwork=1);
+e&&oGd.$Plantern[A+"_"+B]&&(e.highwork=1);
 	        }
 	}
 		e.InitTrigger(e, b, e.R = h, e.C = a, e.AttackedLX = k + e.beAttackedPointL, e.AttackedRX = k + e.beAttackedPointR);
