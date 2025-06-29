20345a3aea1dc006896039d306ba62c72a82ad95
jspvztiiiiiiiii <1825121802@qq.com>
2025-06-29 18:00:15 +0800

    Update CPlants.js

--- jspvz_rtty/js/CPlants.js
+++ jspvz_rtty/js/CPlants.js
@@ -60,7 +60,6 @@
 	let A= (e.R - 1) ||A=(e.R)||A=(e.R+1);
 		let B= (e.C - 1) ||B=(e.C)||B=(e.C+1);
 e&&oGd.$Plantern[A+"_"+B]&&(e.highwork=1);
-	        }
 		e.InitTrigger(e, b, e.R = h, e.C = a, e.AttackedLX = k + e.beAttackedPointL, e.AttackedRX = k + e.beAttackedPointR);
 		$P[b] = e;
 		$P.length += 1;
