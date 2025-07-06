c903954e106a7ee6224143d805603eee0b0374a6
jspvztiiiiiiiii <1825121802@qq.com>
2025-07-06 11:36:36 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -3638,6 +3638,7 @@ oNewspaperZombie2= InheritO(OrnIIZombies, {
             if(!a.bool){
 	        for (let i = 0; i <= 0; i++) {
                 oP.SetTimeoutTomZombie([oNewspaperZombie]);
+		oP.SetTimeoutWaterZombie(7,9,2,[oNewspaperZombie]);
 		a.bool=1;
               }
 	}else if(a.HP<=800){
@@ -3647,7 +3648,7 @@ oNewspaperZombie2= InheritO(OrnIIZombies, {
 	    }
 	},
 	PrivateBirth: function() {
-	    try{AppearTombstones(8,9,2)}catch(){}
+	    AppearTombstones(8,9,2)
         },
 		getHurtOrnLost: function(j, a, g, m, c, l, k, i) {
 		var e = this;
