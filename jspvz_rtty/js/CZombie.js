ac04e15f94f49f8cfefac276cb35448a88203d88
jspvztiiiiiiiii <1825121802@qq.com>
2025-07-07 10:26:40 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -57,9 +57,9 @@ var CZombies = function(b, a) {
                 CustomTop: 400 - h.height + h.GetDY()
             })), h.ZX = h.AttackedLX -= d, h.Ele.style.left = Math.floor(h.X -= d) + "px", g = 1)) : g = 1) : g = 1;
             this.PrivateAct && this.PrivateAct(this);
-	var b = NewEle("dHP", "div", "position:absolute;color:#fff;top:"+h.pixelTop+"px;width:100%;font-size:12px", "", EDAll),
+	var b = NewEle("dHP", "div", "position:absolute;color:#fff;top:"+h.pixelTop+"-40px;width:100%;font-size:12px", "", EDAll),
 		a =(h.OrnHP+h.HP);
-		b.innerHTML ='<span>' +a+ "</span>"
+		b.innerHTML ='<span>'+a+"</span>"
             return g
         },
 		ChkActs1: function(g, e, h, d) {
