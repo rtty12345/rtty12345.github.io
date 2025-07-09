c34a99e278c761698d34a698b70d63a5d116b54f
jspvztiiiiiiiii <1825121802@qq.com>
2025-07-09 20:27:09 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -248,7 +248,7 @@ return a;
                                         n[k].ExplosionDie()
                                     }
                                 } while (h++ < g)
-                            })(e.ZX-120, e.R))
+                            })((e.ZX-120), e.R))
                         },
                         [c]))
                 },
