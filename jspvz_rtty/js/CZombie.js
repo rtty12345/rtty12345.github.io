8a5d57f983debdc369341441b6983ad25be1e3e2
jspvztiiiiiiiii <1825121802@qq.com>
2025-08-17 15:37:45 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -3014,12 +3014,12 @@ oNewspaperZombie = InheritO(OrnIIZombies,{
 	LostPaperSpeed: 32,
 	PicArr: (function() {
 		var a = "images/Zombies/NewspaperZombie/";
-		return ["images/Card/Zombies/NewspaperZombie.png"， a + "0.gif"， a + "HeadWalk1.gif"， a + "HeadAttack1.gif"， a + "LostHeadWalk1.gif"， a + "LostHeadAttack1.gif"， a + "HeadWalk0.gif"， a + "HeadAttack0.gif"， a + "LostHeadWalk0.gif"， a + "LostHeadAttack0.gif"， a + "Head.gif" + $Random， a + "Die.gif" + $Random， a + "BoomDie.gif" + $Random， a + "LostNewspaper.gif"， a + "1.gif"]
+		return ["images/Card/Zombies/NewspaperZombie.png",a + "0.gif",a + "HeadWalk1.gif",a + "HeadAttack1.gif",a + "LostHeadWalk1.gif",a + "LostHeadAttack1.gif",a + "HeadWalk0.gif",a + "HeadAttack0.gif",a + "LostHeadWalk0.gif",a + "LostHeadAttack0.gif",a + "Head.gif" + $Random, a + "Die.gif" + $Random,a + "BoomDie.gif" + $Random, a + "LostNewspaper.gif",a + "1.gif"]
 	})(),
 	AudioArr: ["newspaper_rarrgh2"],
 	Produce: '他的封印只能提供有限的防御<p>韧性：<font color="#FF0000">中（500）</font><br>封印韧性：<font color="#FF0000">低（200）</font><br>伤害：<font color="#FF0000">有报时同普僵，破报后8倍</font><br>速度：正常，而后超级快(失去封印后)</font><br>特性：若在有报时受到灰烬伤害，将自身血量变为900（只能一次),破报愣神时无敌</p>你亲爱的二爷很容易破防，请不要在黑夜关卡用小喷菇惹他',
 	getShadow: function(a) {
-		return "left:75px;top:" + (a。height - 25) + "px"
+		return "left:75px;top:" + (a.height - 25) + "px"
 	},
 	CanPass: function(d, c) {
 		return c
@@ -6341,4 +6341,5 @@ ChkActs1: function(g, e, h, d) {
     g.Stone_of_Sinan_Up = function() {};
   },
 });
+
 
