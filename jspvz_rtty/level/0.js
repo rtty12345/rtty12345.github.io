e5ecf25fe27b255dea2ebb95fff52ffeedd539d1
jspvztiiiiiiiii <1825121802@qq.com>
2025-09-11 20:14:19 +0800

    Update 0.js

--- jspvz_rtty/level/0.js
+++ jspvz_rtty/level/0.js
@@ -1,10 +1,13 @@
 oS.Init({
 	PicArr: function() {
 		var b = $User.Browser.IE6 ? 8 : 32,a = "images/interface/";
-		return [ShadowPNG, a + "Sun.gif", a + "LogoWord.jpg", a + "ZombieHand.png", a + "OptionsMenuback" + b + ".png", a + "OptionsBackButton" + b + ".png", a + "Sunflower_trophy" + b + ".png", a + "Surface.jpg", a + "Help.png", a + "SelectorScreenStartAdventur.png", a + "SelectorScreen_WoodSign3_" + b + ".png", a + "SelectorScreen_WoodSign2_" + b + ".png", a + "SelectorScreen_WoodSign1_" + b + ".png", a + "SelectorScreenAdventure_" + b + ".png", a + "SelectorScreenSurvival_" + b + ".png", a + "SelectorScreen_Almanac_" + b + ".png", a + "SelectorScreenChallenges.png", a + "Logo.jpg", a + "LawnCleaner.png", a + "ZombiesWon.png", a + "LargeWave.gif", a + "FinalWave.gif", a + "PrepareGrowPlants.png", a + "PointerUP.gif", a + "PointerDown.gif", a + "Shovel.png", a + "SunBack.png", a + "ShovelBack.png", a + "GrowSoil.gif", a + "GrowSpray.gif", a + "SeedChooser_Background.png", a + "Button.png", a + "LoadBar.png", a + "Almanac_IndexBack.jpg", a + "Almanac_IndexButton.png", a + "Almanac_CloseButton.png", a + "Almanac_PlantBack.jpg",a + "Almanac_PlantCard.png",a + "Almanac_ZombieBack.jpg", a + "Almanac_ZombieCard.png", a + "ZombiesWon.png", a + "AwardScreen_Back.jpg", a + "trophy.png", a + "splash.png", a + "dialog_header.png", a + "dialog_topleft.png", a + "dialog_topmiddle.png", a + "dialog_topright.png", a + "dialog_centerleft.png", a + "dialog_centerright.png", a + "dialog_bottomleft.png", a + "dialog_bottommiddle.png", a + "dialog_bottomright.png", a + "brain.png", a + "AjaxLoader.gif", a + "SelectorScreenStartAdventur.png",a + "Almanac_IndexBack.png", a + "bengji.gif", a + "SelectorScreen_Shadow_Adventure.png", a + "SelectorScreen_Shadow_Survival.png", a + "SelectorScreen_Shadow_Challenge.png", a + "background3_2.gif", "images/Zombies/NewspaperZombie/1.gif"]
+		return [ShadowPNG, a + "Sun.gif", a + "LogoWord.jpg", a + "ZombieHand.png", a + "OptionsMenuback" + b + ".png", a + "OptionsBackButton" + b + ".png", a + "Sunflower_trophy" + b + ".png", a + "Surface.jpg", a + "Help.png", a + "SelectorScreenStartAdventur.png", a + "SelectorScreen_WoodSign3_" + b + ".png", a + "SelectorScreen_WoodSign2_" + b + ".png", a + "SelectorScreen_WoodSign1_" + b + ".png", a + "SelectorScreenAdventure_" + b + ".png", a + "SelectorScreenSurvival_" + b + ".png", a + "SelectorScreen_Almanac_" + b + ".png", a + "SelectorScreenChallenges.png", a + "Logo.jpg", a + "LawnCleaner.png", a + "ZombiesWon.png", a + "LargeWave.gif", a + "FinalWave.gif", a + "PrepareGrowPlants.png", a + "PointerUP.gif", a + "PointerDown.gif", a + "Shovel.png", a + "SunBack.png", a + "ShovelBack.png", a + "GrowSoil.gif", a + "GrowSpray.gif", a + "SeedChooser_Background.png", a + "Button.png", a + "LoadBar.png", a + "Almanac_IndexBack.jpg", a + "Almanac_IndexButton.png", a + "Almanac_CloseButton.png", a + "Almanac_PlantBack.jpg",a + "Almanac_PlantCard.png",a + "Almanac_ZombieBack.jpg", a + "Almanac_ZombieCard.png", a + "ZombiesWon.png", a + "AwardScreen_Back.jpg", a + "trophy.png", a + "splash.png", a + "dialog_header.png", a + "dialog_topleft.png", a + "dialog_topmiddle.png", a + "dialog_topright.png", a + "dialog_centerleft.png", a + "dialog_centerright.png", a + "dialog_bottomleft.png", a + "dialog_bottommiddle.png", a + "dialog_bottomright.png", a + "brain.png", a + "AjaxLoader.gif", a + "SelectorScreenStartAdventur.png",
+				a + "Almanac_IndexBack.png", a + "bengji.gif", a + "SelectorScreen_Shadow_Adventure.png", a + "SelectorScreen_Shadow_Survival.png", a + "SelectorScreen_Shadow_Challenge.png", a + "background3_2.gif", "images/Zombies/NewspaperZombie/1.gif"]
 	}(),
 	LevelName: "游戏初始界面",
 	LevelEName: 0,
+	PName: [CPlants],
+	ZName: [CZombies],
 	ShowScroll: 1,
 	LoadMusic: "Faster",
 	StartGameMusic: "Faster",
@@ -63,5 +66,6 @@ oS.Init({
 		)();
 	}
 });
+
 
 
