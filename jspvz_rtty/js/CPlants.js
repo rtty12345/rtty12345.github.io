4a07126cb6862ebb41afd06c707699e70e75924b
jspvztiiiiiiiii <1825121802@qq.com>
2025-09-13 11:42:56 +0800

    Update CPlants.js

--- jspvz_rtty/js/CPlants.js
+++ jspvz_rtty/js/CPlants.js
@@ -2565,14 +2565,15 @@ oHypnoShroom = InheritO(oFumeShroom, {
 	HP:1000,
 	PicArr: ["images/Card/Plants/HypnoShroom.png", "images/Plants/HypnoShroom/0.gif", "images/Plants/HypnoShroom/HypnoShroom.gif", "images/Plants/HypnoShroom/HypnoShroomSleep.gif"],
 	Tooltip: "让一只僵尸为你作战",
-	Produce: '当僵尸吃下魅惑菇后，他将会掉转方向为你作战，每隔40秒召唤魅惑玉米普通僵尸(40%)/普通二爷(30%)/机枪铁门(13%)/普通橄榄(9%)/寒冰射手铁门(6%)/玉米炮二爷(2%)<p>使用方法：<font color="#FF0000">单独使用，接触生效</font><br>特点：<font color="#FF0000">让一只僵尸为你作战<br>白天睡觉</font></p>魅惑菇声称：“僵尸们是我们的朋友，他们被严重误解了，僵尸们在我们的生态环境里扮演着重要角色。我们可以也应当更努力地让他们学会用我们的方式来思考。”',
+	Produce: '当僵尸吃下魅惑菇后，他将会掉转方向为你作战，每隔40秒召唤魅惑玉米普通僵尸(25%)/普通二爷(20%)/仙人掌撑杆(17%)/机枪铁门(13%)/普通橄榄(10%)/寒冰射手铁门(10%)/玉米炮二爷(5%)<p>使用方法：<font color="#FF0000">单独使用，接触生效</font><br>特点：<font color="#FF0000">让一只僵尸为你作战<br>白天睡觉</font></p>魅惑菇声称：“僵尸们是我们的朋友，他们被严重误解了，僵尸们在我们的生态环境里扮演着重要角色。我们可以也应当更努力地让他们学会用我们的方式来思考。”',
 	InitTrigger: function() {},
 	PrivateBirth:function(b){
-!b.Sleep&&b.HP>=1&&CustomZombies(Math.round(Math.random()*100)>60 ? new oZombie2
-	      :Math.round(Math.random()*60)>30 ? new oNewspaperZombie
-	      :Math.round(Math.random()*30)>17 ? new oScreenDoorZombie
-	      :Math.round(Math.random()*17)>8 ? new oFootballZombie
-	      :Math.round(Math.random()*8)>2 ? new oPeaZombie
+!b.Sleep&&b.HP>=1&&CustomZombies(Math.round(Math.random()*100)>75 ? new oZombie2
+	      :Math.round(Math.random()*75)>55 ? new oNewspaperZombie
+	      :Math.round(Math.random()*55)>38 ? new oPoleVaultingZombie2
+		  :Math.round(Math.random()*38)>25 ? new oScreenDoorZombie
+	      :Math.round(Math.random()*25)>15 ? new oFootballZombie
+	      :Math.round(Math.random()*15)>5? new oPeaZombie
 	      :new oNewspaperZombie3,b.R,b.C,1);
 	b.HP >= 1&& oSym.addTask(4500,arguments.callee,[b]);
 	},
@@ -3439,31 +3440,3 @@ oFlowerVase = InheritO(CPlants, {
 		return true;
 	}
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
