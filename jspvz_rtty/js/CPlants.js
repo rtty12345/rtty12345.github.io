26008a80d5b82a5cc9d18d2108f391ba1c7a6fbf
jspvztiiiiiiiii <1825121802@qq.com>
2025-09-20 07:29:42 +0800

    Update CPlants.js

--- jspvz_rtty/js/CPlants.js
+++ jspvz_rtty/js/CPlants.js
@@ -2311,7 +2311,7 @@ oGloomShroom = InheritO(oFumeShroom, {
 		l = j + "_Bullet";
 		for (g = k.MinR; g <= f; g++){
 			e = oZ.getArZ(c, b, g);
-			for (h = e.length; h--; (a = e[h]).Altitude < 2 && (a.getHit1(a, 100),a.getr(a,10))
+			for (h = e.length; h--; (a = e[h]).Altitude < 2 && (a.getHit1(a, 80),a.getPoison(a,10))
 			){}
 		}
 		oSym.addTask(100,
@@ -3410,6 +3410,7 @@ oFlowerVase = InheritO(CPlants, {
 		return true;
 	}
 });
+
 
 
 
