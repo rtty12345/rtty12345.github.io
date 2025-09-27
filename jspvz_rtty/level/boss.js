8b22bd45020ce03c0ba3faaf5da3719860af5290
jspvztiiiiiiiii <1825121802@qq.com>
2025-09-27 20:24:16 +0800

    Update boss.js

--- jspvz_rtty/level/boss.js
+++ jspvz_rtty/level/boss.js
@@ -1,5 +1,15 @@
+var oGraveBuster1=InheritO(oGraveBuster, {
+EName: "oGraveBuster1",	
+SunNum:100,
+coolTime:2.5
+});
+var oCoffeeBean1=InheritO(oCoffeeBean, {
+EName: "oCoffeeBean1",	
+SunNum:100,
+coolTime:2.5
+});
 oS.Init({
-PName:[oPeashooter,oSunFlower,oCherryBomb,oWallNut,oPotatoMine,oSnowPea,oChomper,oRepeater,oPuffShroom,oSunShroom,oFumeShroom,oGraveBuster,oHypnoShroom,oScaredyShroom,oIceShroom,oDoomShroom,oLilyPad,oSquash,oThreepeater,oTangleKelp,oJalapeno,oSpikeweed,oTorchwood,oTallNut,oSeaShroom,oPlantern,oCactus,oBlover,oSplitPea,oStarfruit,oPumpkinHead,oFlowerPot,oCoffeeBean,oGarlic,oGatlingPea,oGloomShroom,oTwinSunflower,oSpikerock],
+PName:[oPeashooter,oSunFlower,oCherryBomb,oWallNut,oPotatoMine,oSnowPea,oChomper,oRepeater,oPuffShroom,oSunShroom,oFumeShroom,oGraveBuster1,oHypnoShroom,oScaredyShroom,oIceShroom,oDoomShroom,oLilyPad,oSquash,oThreepeater,oTangleKelp,oJalapeno,oSpikeweed,oTorchwood,oTallNut,oSeaShroom,oPlantern,oCactus,oBlover,oSplitPea,oStarfruit,oPumpkinHead,oFlowerPot,oCoffeeBean1,oGarlic,oGloomShroom,oTwinSunflower,oSpikerock],
 ZName:[oZombie,oZombie2,oZombie3,oNewspaperZombie,oScreenDoorZombie,oFootballZombie,oImp,oNewspaperBoss],
 PicArr:["images/interface/background1.jpg"],
 backgroundImage:"images/interface/background1.jpg",
@@ -7,7 +17,7 @@ CanSelectCard:1,
 SunNum:114514,
 LevelName:"??",
 LvlEName:"boss",
-LargeWaveFlag:{20:$("imgFlag1")},
+LargeWaveFlag:{16:$("imgFlag1")},
 StartGameMusic:"Ultimate battle"
 },
 {
@@ -15,8 +25,8 @@ AZ:[[oNewspaperBoss,0,100,[10]],
    [oZombie,1,0]],
 FlagNum:20,
 FlagToSumNum:{
-    a1: [9],
-		a2: [0,1]},
+    a1: [15],
+	a2: [0,1]},
 FlagToMonitor:{
 20:[ShowFinalWave,0]
 }});
