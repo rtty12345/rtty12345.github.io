4fe23abdcf69c96ab9326e63df4a9c5465e30328
jspvztiiiiiiiii <1825121802@qq.com>
2025-10-02 20:51:43 +0800

    Update 20.js

--- jspvz_rtty/level/20.js
+++ jspvz_rtty/level/20.js
@@ -1,9 +1,7 @@
 oS.Init({
 PName:[oSunFlower,oFumeShroom,oGloomShroom,oPuffShroom,oScaredyShroom,oDoomShroom,oHypnoShroom,oGraveBuster,oIceShroom],
-ZName:[oZombie2,oDancingZombie1,oNewspaperZombie,oJackinTheBoxZombie,oFootballZombie,oScreenDoorZombie,oPeaZombie ,oBackupDancer1],
-PicArr:["images/interface/background2.jpg",
-"images/interface/trophy.png",
-"images/interface/Stripe.png"],
+ZName:[oZombie2,oDancingZombie1,oNewspaperZombie,oJackinTheBoxZombie,oFootballZombie,oScreenDoorZombie,oPeaZombie ,oBackupDancer1,oBackupDancer],
+PicArr:["images/interface/background2.jpg","images/interface/trophy.png","images/interface/Stripe.png"],
 backgroundImage:"images/interface/background2.jpg",
 ShowScroll:false,
 SunNum:600,
@@ -28,20 +26,25 @@ g=f[0],
 b=f[1],
 i=j[0],
 h=j[1],
-a;if(k.Auto){while(i<=h){
+a;
+if(k.Auto){
+while(i<=h){
 CustomSpecial(oBrains,i,0);
-for(a=g;a<=b;a++){CustomSpecial(d[e[c=Math.floor(Math.random()*e.length)]],i,a,1);
-e.splice(c,1)}++i}}NewImg("iStripe","images/interface/Stripe.png","left:"+(GetX1X2(7)[0]-11)+"px;top:65px",EDAll)},
+for(a=g;a<=b;a++){
+CustomSpecial(d[e[c=Math.floor(Math.random()*e.length)]],i,a,1);
+e.splice(c,1)}++i}}
+NewImg("iStripe","images/interface/Stripe.png","left:"+(GetX1X2(7)[0]-11)+"px;top:65px",EDAll)},
 StartGame:function(){
   oP.Monitor();
   BeginCool();
   SetVisible($("dFlagMeter"),$("dFlagMeterContent"),$("dTop"));
-  oS.RiddleAutoGrow();
-  
-}
+  oS.RiddleAutoGrow();  
+  }
 },
-0,{
+},
+{
 AutoSelectCard:function(){
   var c=oS.ArCard,b=-1,a=c.length-1;while(++b<a){SelectCard(c[b].prototype.EName)}
 }
 });
+
