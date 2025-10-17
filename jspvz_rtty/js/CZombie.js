e763ca43d0d5198e9aeb51af76833848d532b049
jspvztiiiiiiiii <1825121802@qq.com>
2025-10-17 23:57:05 +0800

    Update CZombie.js

--- jspvz_rtty/js/CZombie.js
+++ jspvz_rtty/js/CZombie.js
@@ -2541,7 +2541,7 @@ oBucketheadZombie= InheritO(oConeheadZombie,{
             for (w = 0; w < A.length; w++) {
               var t = A[w],
                 s;
-              if ((t.CName == "铁桶僵尸" || t.CName == "领带僵尸" || t.CName == "路障僵尸" || t.CName == "铁栅门僵尸"||t.CName == "辣椒僵尸"||t.CName == "寒冰射手僵尸"||||t.CName == "鸭子救生圈僵尸"||t.CName=="路障鸭子救生圈僵尸"||t.CName== "铁桶鸭子救生圈僵尸")&&
+              if ((t.CName == "铁桶僵尸" || t.CName == "领带僵尸" || t.CName == "路障僵尸" || t.CName == "铁栅门僵尸"||t.CName == "辣椒僵尸"||t.CName == "寒冰射手僵尸"||t.CName == "鸭子救生圈僵尸"||t.CName=="路障鸭子救生圈僵尸"||t.CName== "铁桶鸭子救生圈僵尸")&&
 			(t.OrnHP<1)&&(t.beAttacked)&&($Z[t.id])&&(t.PZ==a.PZ)) {
 			    t&&(t.OrnHP = 1100,
                   t.getHit0 = (s = OrnIZombies.prototype).getHit0,
@@ -6520,6 +6520,7 @@ ChkActs1: function(g, e, h, d) {
     g.Stone_of_Sinan_Up = function() {};
   },
 });
+
 
 
 
