8078805958e72deb1fadcaa378a177b9c7cec5d5
jspvztiiiiiiiii <1825121802@qq.com>
2025-10-18 09:07:46 +0800

    Update zombie list in ImZombiePool1.js

--- jspvz_rtty/level/ImZombiePool1.js
+++ jspvz_rtty/level/ImZombiePool1.js
@@ -4,7 +4,7 @@
 */
 oS.Init({
         PName: [oFumeShroom, oPumpkinHead, oHypnoShroom, oPotatoMine, oSunFlower, oThreepeater, oGatlingPea, oCactus, oPeashooter,oRepeater,oJalapeno,oGarlic,oPlantern],
-        ZName: [oZombie2,oBucketheadZombie,oDuckyTubeZombie2, oDuckyTubeZombie3, oJackinTheBoxZombie, oBalloonZombie, oNewspaperZombie3,oFlagZombie,oSnorkelZombie,oDolphinRiderZombie],
+        ZName: [oZombie2,oBucketheadZombie,oDuckyTubeZombie2, oDuckyTubeZombie3, oJackinTheBoxZombie, oBalloonZombie, oNewspaperZombie3,oFootballZombie1,oSnorkelZombie,oDolphinRiderZombie],
         PicArr: ["images/interface/background4.jpg", "images/interface/trophy.png", "images/interface/Stripe.png"],
         backgroundImage: "images/interface/background4.jpg",
         Coord: 2, DKind: 0, LF: [0, 1, 1, 2, 2, 1, 1],
