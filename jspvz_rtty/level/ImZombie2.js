3ad94447c85f4484f30f5685824bb129a3c4ecdb
jspvztiiiiiiiii <1825121802@qq.com>
2025-11-08 17:21:08 +0800

    Update ImZombie2.js

--- jspvz_rtty/level/ImZombie2.js
+++ jspvz_rtty/level/ImZombie2.js
@@ -1,6 +1,6 @@
 oS.Init({
   PName: [oPeashooter, oSunFlower, oSnowPea, oSpikeweed, oGloomShroom, oStarfruit, oGatlingPea, oTorchwood, oRepeater, oPotatoMine, oWallNut, oSpikerock, oThreepeater, oGarlic, oCactus, oChomper, oSquash, oScaredyShroom, oTallNut, oBlover, oHypnoShroom],
-  ZName: [oNewspaperZombie, oJalapenoZombie,oZombie3, oFlagZombie, oNewspaperZombie2,oZomboni, oConeheadZombie, oDancingZombie2, oPeaZombie, oPoleVaultingZombie],
+  ZName: [oNewspaperZombie, oJalapenoZombie,oZombie3, oFlagZombie, oNewspaperZombie2,oZomboni, oConeheadZombie, oDancingZombie2, oPeaZombie, oPoleVaultingZombie,oJackinTheBoxZombie],
   PicArr: ["images/interface/background2.jpg", "images/interface/trophy.png", "images/interface/Stripe.png"],
   backgroundImage: "images/interface/background2.jpg",
   ShowScroll: false,
@@ -20,6 +20,41 @@ oS.Init({
     Auto: 1,
     P: [0, 1, 1, 1, 20, 2, 3, 3, 4, 4, 5, 5, 6, 6, 19, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 14, 14, 15, 16, 16, 17, 17, 18]
   },
+  LvlClearFunc: function() {
+                oS.ScrollScreen = oS.LvlVar.ScrollScreen;
+                delete oS.LvlVar.ScrollScreen
+  },
+    LoadAccess: function(i) {
+           oS.LvlVar.ScrollScreen = oS.ScrollScreen;
+            $("tGround").style.left = "-115px";
+            ClearChild($("JSPVZAjax"));
+            oS.RiddleAutoGrow();
+            SetStyle($("dTop"), {
+                left: "105px",
+                top: 0,
+                visibility: "visible"
+            });
+            innerText(ESSunNum, oS.SunNum);
+            oS.ScrollScreen = function() {
+                $("tGround").style.left = 0;
+                ClearChild($("dButton1"), $("dButton2"));
+                (function() {
+                    (EDAll.scrollLeft += 25) < 500 ? oSym.addTask(2, arguments.callee, []) : SetVisible($("dMenu"), $("dSelectCard"), $("dCardList"));
+                })();
+            };
+        NewEle("dButton1", "button", "position:absolute;left:650px;top:510px;width:100px;height:35px;z-index:255", {
+                innerHTML: "开始挑战",
+                onclick: function() {
+                    i(0);
+                }
+            }, EDAll);
+            NewEle("dButton2", "button", "position:absolute;left:760px;top:510px;width:100px;height:35px;z-index:255", {
+                innerHTML: "不玩这个",
+                onclick: function() {
+                    SelectModal(0);
+                }
+            }, EDAll);
+        },
   RiddleAutoGrow: function() {
     var k = oS.ArP,
       f = k.ArC,
@@ -45,7 +80,7 @@ oS.Init({
   StartGame: function() {
     oP.Monitor();
     BeginCool();
-    SetVisible($("dFlagMeter"), $("dFlagMeterContent"), $("dTop"));
+    SetVisible($("dFlagMeter"), $("dFlagMeterContent"));
     oS.RiddleAutoGrow()
   }
 });
