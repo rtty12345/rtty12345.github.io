bf160c4abe094630de4144cfe5fd3b4693902c0a
jspvztiiiiiiiii <1825121802@qq.com>
2025-11-08 20:28:18 +0800

    Update ImZombie2.js

--- jspvz_rtty/level/ImZombie2.js
+++ jspvz_rtty/level/ImZombie2.js
@@ -1,6 +1,6 @@
 oS.Init({
   PName: [oPeashooter, oSunFlower, oSnowPea, oSpikeweed, oGloomShroom, oStarfruit, oGatlingPea, oTorchwood, oRepeater, oPotatoMine, oWallNut, oSpikerock, oThreepeater, oGarlic, oCactus, oChomper, oSquash, oScaredyShroom, oTallNut, oBlover, oHypnoShroom],
-  ZName: [oNewspaperZombie, oJalapenoZombie,oZombie3, oFlagZombie, oNewspaperZombie2,oZomboni, oConeheadZombie, oDancingZombie2, oPeaZombie, oPoleVaultingZombie,oJackinTheBoxZombie],
+  ZName: [oNewspaperZombie, oJalapenoZombie,oZombie3, oFlagZombie, oNewspaperZombie2,oZomboni, oConeheadZombie, oDancingZombie2, oPeaZombie, oPoleVaultingZombie,oJackinTheBoxZombie,oFootballZombie1,oPoleVaultingZombie1],
   PicArr: ["images/interface/background2.jpg", "images/interface/trophy.png", "images/interface/Stripe.png"],
   backgroundImage: "images/interface/background2.jpg",
   SunNum: 2000,
