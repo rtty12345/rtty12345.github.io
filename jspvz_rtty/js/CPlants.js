c85ff2f65d4bb5521bc5993df4cf88def5b6b416
jspvztiiiiiiiii <1825121802@qq.com>
2026-01-10 08:08:04 +0800

    Update CPlants.js

--- jspvz_rtty/js/CPlants.js
+++ jspvz_rtty/js/CPlants.js
@@ -1854,7 +1854,7 @@ oTallNut = InheritO(oWallNut, {
                 d = $(c.id).childNodes[1];
     if(b%3){var a=1000}
     (c.HP -= a) < 1 ? (c.Die(),CustomSpecial(oNutBowling,c.R,c.C)): c.HP < 3333 ? c.HurtStatus < 2 && (c.HurtStatus = 2, d.src = "images/Plants/TallNut/TallnutCracked2.gif",CustomSpecial(oNutBowling,c.R,c.C)) : c.HP < 6666 && c.HurtStatus < 1 && (c.HurtStatus = 1, d.src = "images/Plants/TallNut/TallnutCracked1.gif",CustomSpecial(oBoomNutBowling,c.R,c.C))
-            e.HP&&(b!==3)&&e.getHit1(e,Math.min(a*0.3,30),0);
+            e.HP&&(b!==3)&&e.getHit1(e,Math.max(a*0.3,30),0);
         },
 	DieClear:function(){}
 }),
@@ -3562,6 +3562,7 @@ oFlowerVase = InheritO(CPlants, {
 		return true;
 	}
 })
+
 
 
 
